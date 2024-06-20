b7f7432c4e72eeff6bb77b7f5c84ec4eda6a9aee
Hemal Bhatti <bhattihemal004@gmail.com>
2024-06-20 13:22:19 +0530

    basic set-up of password generator project

---
+++ 04passwordGenerator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1> Basic set of project !! </h1>
+  )
+}
+
+export default App
